de36c40072f0ba1e76d9c5444a03ff7426b2e9da
kasvoy <woj.kasperczyk@gmail.com>
2024-01-20 15:10:54 +0100

    Proper split of rotten reviews

--- dataset_utils.py
+++ dataset_utils.py
@@ -3,7 +3,6 @@ import re
 import random
 
 import pandas as pd
-import numpy as np
 
 IMDB_DATA_PATH        = "data/aclImdb/"
 POLARITY_v1_DATA_PATH = "data/rt-polaritydata/rt-polaritydata/"
@@ -45,6 +44,9 @@ def load_imdb_dataset(train_path: str, test_path: str, seed=1) -> tuple:
     train_texts = [re.sub('<.*?>', '', text) for text in train_texts]
     test_texts  = [re.sub('<.*?>', '', text) for text in test_texts]
     
+    train_texts = [text.lower() for text in train_texts]
+    test_texts  = [text.lower() for text in test_texts]
+    
     return ((train_texts, train_labels), (test_texts, test_labels))
 
 
@@ -69,60 +71,10 @@ def load_polarity(v1_path, v2_path):
     (v2_texts, v2_labels) = load_inidv_dataset(set_path=v2_path)
     
     #removing newline tags present in the polarity texts
-    v1_texts = [text.replace('\n', ' ') for text in v1_texts]
-    v2_texts = [text.replace('\n', ' ') for text in v2_texts]
+    v1_texts = [text.replace('\n', ' ').lower() for text in v1_texts]
+    v2_texts = [text.replace('\n', ' ').lower() for text in v2_texts]
     
     return ((v1_texts, v1_labels), (v2_texts, v2_labels))
-
-
-def load_rotten(rotten_path, seed=1):
-    df = pd.read_csv(ROTTEN_PATH).drop_duplicates()
-    df = df[['review_type', 'review_content']]
-
-    #getting only non empty reviews
-    df = df.loc[df['review_content'].notna()]
-
-    #removing the reviews that ask the reader to click somewhere else
-    df = df.loc[~df['review_content'].str.contains('full review|click for review|read review')]
-
-    df['review_type'].replace({'Fresh': 1, 'Rotten': 0}, inplace=True)
-    df.rename(columns={'review_type': 'positive_review'}, inplace=True)
-
-    is_positive = df['positive_review'] == 1
-    pos_df = df.loc[is_positive]
-    neg_df = df.loc[~is_positive]
-
-    pos_texts = pos_df['review_content'].tolist()
-    neg_texts = neg_df['review_content'].tolist()
-
-    pos_25 = random.sample(pos_texts, 25000)
-    neg_25 = random.sample(neg_texts, 25000)
-
-    train_pos = list(zip(pos_25[:12500], [1 for _ in range(12500)]))
-    test_pos  = list(zip(pos_25[12500:], [1 for _ in range(12500)]))
-    train_neg = list(zip(neg_25[:12500], [0 for _ in range(12500)]))
-    test_neg  = list(zip(neg_25[12500:], [0 for _ in range(12500)]))
-
-    train_set = train_pos + train_neg
-    test_set  = test_pos + test_neg
-
-    train_texts, train_labels = zip(*train_set)
-    test_texts, test_labels = zip(*test_set)
-
-    train_texts = list(train_texts)
-    train_labels = list(train_labels)
-    test_texts = list(test_texts)
-    test_labels = list(test_labels)
-
-    random.seed(seed)
-    random.shuffle(train_texts)
-    random.shuffle(test_texts)
-
-    random.seed(seed)
-    random.shuffle(train_labels)
-    random.shuffle(test_labels)
-    
-    return ((train_texts, train_labels), (test_texts, test_labels))
 
 
 def display_dataset_info(texts, labels, name):
@@ -131,4 +83,68 @@ def display_dataset_info(texts, labels, name):
     print(f"Positive reviews total: {sum(label==1 for label in labels)}")
     print(f"Negative reviews total: {sum(label==0 for label in labels)}")
     
+    
+def load_rotten_split(rotten_path, seed=1):
+    
+    """
+    Utility function for loading the rotten tomatoes critic dataset.
+    
+    
+    Returns:
+    
+    Training set: Two lists, one for texts, one for labels. Needed for training in this form.
+    
+    Test set: Multiple test sets, all as lists of (label, text tuples). Reason for difference in loading is the fact 
+    that the test set will be split later in the program, and with tuples the labels are there after a split, with no need of
+    reassignment.
+    """
+    
+    df = pd.read_csv(rotten_path).drop_duplicates()
+    df = df[['review_type', 'review_content']]
 
+    #getting only non empty reviews
+    df = df.loc[df['review_content'].notna()]
+    
+    #lowercasing all texts
+    df['review_content'] = df['review_content'].str.lower()
+
+    #removing the reviews that ask the reader to click somewhere else
+    df = df.loc[~df['review_content'].str.contains('full review|click for review|read review')]
+
+    df['review_type'].replace({'Fresh': 1, 'Rotten': 0}, inplace=True)
+    df.rename(columns={'review_type': 'positive_review'}, inplace=True)
+    
+    is_positive_mask = df['positive_review'] == 1
+    pos_reviews_train_df = df[is_positive_mask].sample(n=25000, random_state=42)    
+    neg_reviews_train_df = df[~is_positive_mask].sample(n=25000, random_state=42)
+    
+    #dropping already sampled texts, so no repeated reviews across test sets.
+    df.drop(neg_reviews_train_df.index, inplace=True)
+    df.drop(pos_reviews_train_df.index, inplace=True)
+    
+    #using text.split(" ") on the space to represent a token as it closely resembles the output of a dedicated tokenizer (e.g. within spacy)
+    #and counts the number of tokens in this way incomparably faster compared if we had used spacy.
+    short_reviews_test_df = df[df['review_content'].apply(lambda x: len(x.split(" ")) < 25)].sample(n=10000, random_state=42)
+    df.drop(short_reviews_test_df.index, inplace=True)
+    
+    random_reviews_test_df = df.sample(n=10000, random_state=42)
+
+
+    #(label, text) tuples
+    short_reviews_test = list(zip(short_reviews_test_df['positive_review'], short_reviews_test_df['review_content']))
+    pos_reviews_train  = list(zip(pos_reviews_train_df['positive_review'], pos_reviews_train_df['review_content']))
+    neg_reviews_train  = list(zip(neg_reviews_train_df['positive_review'], neg_reviews_train_df['review_content']))
+    random_reviews_test= list(zip(random_reviews_test_df['positive_review'], random_reviews_test_df['review_content']))
+    
+    rotten_train = pos_reviews_train + neg_reviews_train
+    
+    random.seed(seed)
+    random.shuffle(rotten_train)
+    random.shuffle(short_reviews_test)
+    random.shuffle(random_reviews_test)
+        
+    rotten_train_texts = [text for _, text in rotten_train]
+    rotten_train_labels = [label for label,_ in rotten_train]
+    
+
+    return (rotten_train_texts, rotten_train_labels, short_reviews_test, random_reviews_test)
